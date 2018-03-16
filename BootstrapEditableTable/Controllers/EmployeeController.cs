3d997e2f80086b37501cc2082eb5eac168983604
prayashsharma <prayashsharma@live.com>
2018-03-16 18:08:45 -0400

    Add project files.

---
+++ BootstrapEditableTable/Controllers/EmployeeController.cs
@@ -0,0 +1,37 @@
+﻿using BootstrapEditableTable.Models;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace BootstrapEditableTable.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetAll()
+        {
+            var employees = new List<Employee>()
+            {
+                new Employee {Id = 1, Name = "John Doe", Title ="Manager" },
+                new Employee {Id = 2, Name = "Jenny Doe", Title ="Programmer" },
+                new Employee {Id = 3, Name = "Jason Doe", Title ="Director" },
+                new Employee {Id = 4, Name = "Jack Doe", Title ="Assistant" },
+            };
+            return Json(employees, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public ActionResult Update(List<Employee> employees)
+        {
+
+            return Json(new { Success = true, employees });
+        }
+
+
+    }
+}
---
+++ BootstrapEditableTable/Models/Employee.cs
@@ -0,0 +1,9 @@
+﻿namespace BootstrapEditableTable.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Title { get; set; }
+    }
+}
